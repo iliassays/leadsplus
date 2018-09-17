da30f2beb331a48ce899be7f533d687611ed89f3
Ilias Hossain <shimulsays@gmail.com>
2018-09-17 16:26:05 +0600

    added missing file

---
+++ src/Services/Contact/Contact.Domain/Queries/GetAllContactQuery.cs
@@ -0,0 +1,29 @@
+﻿namespace Contact.Domain.Query
+{
+    using LeadsPlus.Core;
+    using LeadsPlus.Core.Query;
+    using MongoDB.Bson;
+    using MongoDB.Driver;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public class GetAllContactQuery
+    {
+
+    }
+
+    public class GetAllContactQueryHandler : IQueryHandler<GetAllContactQuery, List<Contact>>
+    {
+        private readonly IRepository<Contact> contactRepository;
+
+        public GetAllContactQueryHandler(IRepository<Contact> contactRepository)
+        {
+            this.contactRepository = contactRepository;
+        }
+
+        public async Task<List<Contact>> Handle(GetAllContactQuery query)
+        {
+            return await contactRepository.Collection.Find(new BsonDocument()).ToListAsync();
+        }
+    }
+}
