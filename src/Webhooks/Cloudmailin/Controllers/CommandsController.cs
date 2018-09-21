42d39f959e64f38fc9e729b6913432ec9edba6c6
Shibly <shibly77@outlook.com>
2018-09-21 16:57:26 +0600

    Zapier webhook.

--- src/Webhooks/Cloudmailin/Controllers/CommandsController.cs
+++ src/Webhooks/Cloudmailin/Controllers/CommandsController.cs
@@ -1,16 +1,14 @@
 ﻿namespace Cloudmailin.Webhook.Controllers
 {
-    using Microsoft.AspNetCore.Authorization;
-    using Microsoft.AspNetCore.Mvc;
-    using System;
-    using System.Net;
-    using System.Threading.Tasks;
+    using Cloudmailin.IntegrationEvents;
     using Cloudmailin.Webhook.Command;
     using Cloudmailin.Webhook.IntegrationEvents;
     using LeadsPlus.BuildingBlocks.EventBus.Abstractions;
-    using System.Net.Http.Formatting;
+    using Microsoft.AspNetCore.Mvc;
     using Microsoft.Extensions.Logging;
     using Newtonsoft.Json;
+    using System;
+    using System.Threading.Tasks;
 
     [Route("api/v1/[controller]")]
     //[Authorize]
@@ -22,6 +20,7 @@
         public CommandsController(IEventBus eventBus, ILoggerFactory logger)
         {
             this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
             _eventBus = eventBus ?? throw new ArgumentNullException(nameof(eventBus));
         }
 
@@ -32,29 +31,72 @@
             return Ok("Say Hello");
         }
 
-        [Route("parse")]
+        [Route("tracked")]
         [HttpPost]
-        public async Task<IActionResult> Parse([FromBody] CreateInboundEmailCommand createInboundEmailCommand)
+        public async Task<IActionResult> Tracked([FromBody] CreateInboundEmailCommand createInboundEmailCommand)
         {
-            dynamic form = Request.Form["envelope"];
 
-            var @event = new AgentInboundEmailTrackedIntegrationEvent()
+            var requestForm = Request.Form;
+            var form = JsonConvert.SerializeObject(Request.Form);
+            var logr = logger.CreateLogger("Tracked");
+
+            logr.Log(LogLevel.Critical, $"Full email  : { form }");
+
+            var from = requestForm["envelope[from]"];
+            logr.Log(LogLevel.Critical, $"from: { from }");
+
+            var to = requestForm["envelope[to]"];
+            logr.Log(LogLevel.Critical, $"to: { to }");
+
+            var body = requestForm["html"];
+            logr.Log(LogLevel.Critical, $"body: { body }");
+
+            var subject = requestForm["headers[Subject]"];
+            subject = $"{subject} :[{to}]";
+            logr.Log(LogLevel.Critical, $"subject: { subject }");
+
+            var zapierMailbox = "rightmove0co0uk0customerinquery@robot.zapier.com";
+
+            var emailNeedsToBeSent = new EmailNeedsToBeSentIntegrationEvent
             {
-                Body = createInboundEmailCommand.Text,
-                PlainText = createInboundEmailCommand.Plain,
-                CustomerEmail = form.from,
-                AgentEmail = form.to,
-                Subject = createInboundEmailCommand.Subject,
-                AggregateId = Guid.NewGuid().ToString()
+                Body = body,
+                IsBodyHtml = true,
+                Subject = subject,
+                FromEmail = from,
+                To = new[] { zapierMailbox },
+                AggregateId = Guid.NewGuid().ToString(),
             };
 
-            logger.CreateLogger(nameof(createInboundEmailCommand)).LogTrace($"New customer email tracked.. agent {createInboundEmailCommand.To}, customer {createInboundEmailCommand.From}.");
+            _eventBus.Publish(emailNeedsToBeSent);
 
-            //This  will trigger event in Agent Api to send a autorespondar
-            _eventBus.Publish(@event);
+            logr.Log(LogLevel.Critical, "Email need to be sent published !");
 
 
-            return (IActionResult)Ok(@event);
+            return (IActionResult)Ok();
         }
+
+        [Route("parsed")]
+        [HttpPost]
+        public async Task<IActionResult> Parsed(string trackedData)
+        {
+            var logr = logger.CreateLogger("Parsed");
+            logr.Log(LogLevel.Critical, "Parsed....");
+
+            try
+            {
+                logr.Log(LogLevel.Critical, $"body : {trackedData}");
+                logr.Log(LogLevel.Critical, $"from : {JsonConvert.SerializeObject(Request.Form)}");
+                var dynamic = JsonConvert.DeserializeObject<dynamic>(trackedData);
+                logr.Log(LogLevel.Critical, $"dynamic : {JsonConvert.SerializeObject(dynamic)}");
+
+            }
+            catch (Exception ex)
+            {
+                logr.Log(LogLevel.Critical, $"exception : {ex.ToString()}");
+            }
+
+            return (IActionResult)Ok();
+        }
+
     }
 }
---
+++ src/Webhooks/Cloudmailin/IntegrationEvents/Events/EmailNeedsToBeSentIntegrationEvent.cs
@@ -0,0 +1,25 @@
+﻿namespace Cloudmailin.IntegrationEvents
+{
+    using LeadsPlus.BuildingBlocks.EventBus.Events;
+    using System.Collections.Generic;
+
+    public class EmailNeedsToBeSentIntegrationEvent : IntegrationEvent
+    {
+        public string[] To { get; set; }
+        public string[] Cc { get; set; }
+        public string FromEmail { get; set; }
+        public string FromName { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public bool IsBodyHtml { get; set; }
+        public string ReplyTo { get; set; }
+        public Dictionary<string, string> MergeFields { get; set; }
+        public string TemplateId { get; set; }
+
+        public EmailNeedsToBeSentIntegrationEvent()
+        {
+
+        }
+    }
+}
+
